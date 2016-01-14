a7780ad4287acf7dedd2fd2acfadf3a5eabb2b94
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-01-13 19:46:53 -0500

    adding menu

---
+++ src/js/editor.js
@@ -0,0 +1,43 @@
+// Import CodeMirror
+import CodeMirror from 'codemirror';
+
+// Import CodeMirror addons and modules
+import 'codemirror/addon/search/searchcursor';
+import 'codemirror/addon/search/search';
+import 'codemirror/addon/dialog/dialog';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/wrap/hardwrap';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/display/rulers';
+import 'codemirror/addon/display/panel';
+import 'codemirror/mode/clike/clike.js';
+
+// Keymap
+import 'codemirror/keymap/sublime';
+
+export function initEditor (container, options) {
+	// CREATE AND START CODEMIRROR
+    let editorDOM = document.createElement('div');
+    editorDOM.setAttribute('class', 'ge_editor');
+    container.appendChild(editorDOM);
+    
+    let cm = CodeMirror(editorDOM, {
+        value: options.frag,
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: 'x-shader/x-fragment',
+        keyMap: 'sublime',
+        autoCloseBrackets: true,
+        extraKeys: { 'Ctrl-Space': 'autocomplete' },
+        showCursorWhenSelecting: true,
+        theme: 'monokai',
+        indentUnit: 4
+    });
+
+    return cm;
+}
