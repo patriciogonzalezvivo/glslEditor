38c39973dc295ec78a5442c311a481b274c6bbc1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-01-30 08:20:24 -0500

    dont save snapshoot

--- src/js/io/serverLog.js
+++ src/js/io/serverLog.js
@@ -8,19 +8,19 @@ export function saveOnServer (ge, callback) {
     }
 
     // STORE A COPY on SERVER
-    // let url = 'http://thebookofshaders.com:8080/';
-    let url = 'http://localhost:8080/';
+    let url = 'http://thebookofshaders.com:8080/';
+    // let url = 'http://localhost:8080/';
     let data = new FormData();
     data.append('code', content);
 
-    let dataURL = ge.sandbox.canvasDOM.toDataURL("image/png");
-    let blobBin = atob(dataURL.split(',')[1]);
-    let array = [];
-    for (let i = 0; i < blobBin.length; i++) {
-        array.push(blobBin.charCodeAt(i));
-    }
-    let file = new Blob([new Uint8Array(array)], {type: 'image/png'});
-    data.append("image", file);
+    // let dataURL = ge.sandbox.canvasDOM.toDataURL("image/png");
+    // let blobBin = atob(dataURL.split(',')[1]);
+    // let array = [];
+    // for (let i = 0; i < blobBin.length; i++) {
+    //     array.push(blobBin.charCodeAt(i));
+    // }
+    // let file = new Blob([new Uint8Array(array)], {type: 'image/png'});
+    // data.append("image", file);
 
     let xhr = new XMLHttpRequest();
     xhr.open('POST', url+'save', true);
