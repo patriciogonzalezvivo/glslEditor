786f3251134e361f3beb988ec1727b3382304523
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-05-13 09:39:44 -0400

    exporting to .edit.php

--- src/js/ui/modals/ExportModal.js
+++ src/js/ui/modals/ExportModal.js
@@ -13,14 +13,14 @@ export default class ExportModal extends Modal {
 
         this.codeURL = new MenuItem(this.el, 'ge_sub_menu', 'Code URL...', (event) => {
             saveOnServer(this.main, (event) => {
-                prompt('Use this url to share your code', 'http://editor.thebookofshaders.com/?log=' + event.name);
+                prompt('Use this url to share your code', 'http://thebookofshaders.com/edit.php?log=' + event.name);
                 this.removeModal();
             });
         });
 
         this.shaderURL = new MenuItem(this.el, 'ge_sub_menu', 'Artwork URL...', (event) => {
             saveOnServer(this.main, (event) => {
-                prompt('Use this url to share your artwork', 'http://player.thebookofshaders.com/?log=' + event.name);
+                prompt('Use this url to share your artwork', 'http://thebookofshaders.com/glslPlayer/?log=' + event.name);
                 this.removeModal();
             });
         });
